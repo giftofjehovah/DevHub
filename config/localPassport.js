89483c2a13fc2001826e54308885e6cbfadcc9a2
gabrielleong <gabrielleong97@gmail.com>
2016-04-11 11:09:22 +0800

    specifies local-login function in localPassport.js

--- config/localPassport.js
+++ config/localPassport.js
@@ -35,4 +35,26 @@ module.exports = function (passport) {
       }
     })
   }))
+
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, done) {
+    User.findOne({ 'email': email }, function (err, user) {
+      if (err) {
+        return done(err)
+      }
+
+      if (!user) {
+        return done(null, false, req.flash('errorMessage', 'No user found!'))
+      }
+
+      if (!user.validPassword(password)) {
+        return done(null, false, req.flash('errorMessage', 'Incorrect Password'))
+      }
+
+      done(null, user)
+    })
+  }))
 }
