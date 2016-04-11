1dde0ac69a1451f1f80193cb5d32c69578034959
giftofjehovah <jonathan.pwh@gmail.com>
2016-04-11 11:58:56 +0800

    add call back to GET test case

--- spec/loginLocalSpec.js
+++ spec/loginLocalSpec.js
@@ -1,11 +1,11 @@
-'use strict'
 const request = require('request')
 
 describe('User can register', function () {
   describe('GET /local/signup', function () {
-    it('should return status 200', function () {
-      request.get('http://localhost:5000/local/signup').on('response', function (response) {
+    it('should return status 200', function (done) {
+      request.get('http://localhost:3000/local/signup').on('response', function (response) {
         expect(response.statusCode).toEqual(200)
+        done()
       })
     })
   })
