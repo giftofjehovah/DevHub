d77342c0912f13e4030ee75dcfb7f217ed388785
gabrielleong <gabrielleong97@gmail.com>
2016-04-11 10:33:00 +0800

    changes local.email to email in config/localPassport.js

--- config/localPassport.js
+++ config/localPassport.js
@@ -19,13 +19,13 @@ module.exports = function (passport) {
     passwordField: 'password',
     passReqToCallback: true
   }, function (req, email, password, done) {
-    User.findOne({ 'local.email': email }, function (err, user) {
+    User.findOne({ 'email': email }, function (err, user) {
       if (err) return done(err)
       if (user) {
         return done(null, false, req.flash('errorMessage', 'This email is already used!'))
       } else {
         var newUser = new User()
-        newUser.local.email = email
+        newUser.email = email
         newUser.local.password = User.encrypt(password)
 
         newUser.save(function (err, user) {
