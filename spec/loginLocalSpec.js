c1fa0e2e4860226469fbfd52667dd8ed324d103a
giftofjehovah <jonathan.pwh@gmail.com>
2016-04-11 12:09:06 +0800

    change to es5

--- spec/loginLocalSpec.js
+++ spec/loginLocalSpec.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 describe('User can register', function () {
   describe('GET /local/signup', function () {
     it('should return status 200', function (done) {
-      request.get(`http://localhost:${port}/local/signup`).on('response', function (response) {
+      request.get('http://localhost:' + port + '/local/signup').on('response', function (response) {
         expect(response.statusCode).toEqual(200)
         done()
       })
