4566480b397d6c96cec9056f4235187a4a7f82e5
ozy8 <owzhiyin@Zhiyin-Ows-MacBook-Air.local>
2016-04-11 09:36:51 +0800

    add companySchema

--- models/company.js
+++ models/company.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose')
+
+const CompanySchema = mongoose.Schema({
+  name: String,
+  image: String,
+  industry: [String],
+  description: String,
+  stack: [String],
+  jobs: [String]
+
+})
+
+module.exports = mongoose.model('Company', CompanySchema)
