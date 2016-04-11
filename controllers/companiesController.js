78bb85e676065d80c84171adc00196ecc1973c07
ozy8 <owzhiyin@Zhiyin-Ows-MacBook-Air.local>
2016-04-11 14:09:56 +0800

    add companyRoutes

--- controllers/companiesController.js
+++ controllers/companiesController.js
@@ -4,7 +4,8 @@ const Company = require('../models/company')
 function getAll (request, response) {
   Company.find((error, companies) => {
     if (error) return response.json({message: 'Company not found'})
-    response.send('Companies displayed')
+    // response.send('Companies displayed')
+    response.json({companies: companies})
   })
 }
 
@@ -13,7 +14,8 @@ function getCompany (request, response) {
   var id = request.params.id
   Company.findById({_id: id}, function (error, company) {
     if (error) response.json({message: 'Could not find company b/c:' + error})
-    response.send('Company displayed')
+    // response.send('Company displayed')
+    response.json({company: company})
   })
 }
 
