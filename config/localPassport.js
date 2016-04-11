294e360d54dce4669c061e1047ab7d317df27012
gabrielleong <gabrielleong97@gmail.com>
2016-04-11 09:41:45 +0800

    finds user by email, else creates new user

---
+++ config/localPassport.js
@@ -0,0 +1,27 @@
+const User = require('../models/user')
+const LocalStrategy = require('passport-local').Strategy
+
+module.exports = function (passport) {
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, done) {
+    User.findOne({ 'local.email': email }, function (err, user) {
+      if (err) return done(err)
+      if (user) {
+        return done(null, false, req.flash('errorMessage', 'This email is already used!'))
+      } else {
+        var newUser = new User()
+        newUser.local.email = email
+        newUser.local.password = User.encrypt(password)
+
+        newUser.save(function (err, user) {
+          if (err) return done(err)
+          return done(null, user)
+        })
+      }
+    })
+  })
+  )
+}
