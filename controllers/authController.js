ff8735a0d3e6c2c8767afe3cb0069f61d44825cb
gabrielleong <gabrielleong97@gmail.com>
2016-04-13 20:49:48 +0800

    fixes redirect of successful auth to profile page

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,9 +7,12 @@ function getGithubLogin (req, res) {
 
 // GET /auth/github/callback
 function githubCallback (req, res) {
-    const username = req.params.username
-    return passport.authenticate('github', { failureRedirect: '/login', successRedirect: '/users/'+ username})(req, res)
+    return passport.authenticate('github', function(err,user,info){
+        if (err) console.log(err)
+        res.redirect('/users/'+user.github.username)
+    })(req,res)
 }
+    
 
 // GET /logout
 function getLogout (req, res) {
