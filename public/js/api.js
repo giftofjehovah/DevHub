b344211f5a89403e5361a6348075401d0a7956f5
giftofjehovah <jonathan.pwh@gmail.com>
2016-04-14 01:40:25 +0800

    add json result to html

--- public/js/api.js
+++ public/js/api.js
@@ -3,18 +3,17 @@ function setHeader (xhr) {
   xhr.setRequestHeader('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImdpZnRvZmplaG92YWgiLCJfaWQiOiI1NzBkYmIzMTkzNWZlZDA1Y2MzNTUxMzMiLCJpYXQiOjE0NjA1NTI4MTJ9.4moKMUqyu5Az59ldE3xPajN0HWiH6YsEmJGlAlFmP4s')
 }
 
-
 $('.btn').on('click', function (event) {
   var form = event.currentTarget.id + 'field'
   var value = $('#' + form).val()
   var load = event.currentTarget.id + 'load'
+  var card = event.currentTarget.id + 'card'
+  $('.code').remove()
   if (!value) {
     console.log('hi')
     $('#' + form).addClass('invalid')
-  }
-  else {
-    $('#' + load).before($('<div>').addClass('progress').append($('<div>').addClass('indeterminate')))
-    // $('<div>').addClass('indeterminate').appendTo($('<div>').addClass('progress')).appendTo($('#' + load))
+  } else {
+    $('<div>').addClass('progress').append($('<div>').addClass('indeterminate')).insertBefore('#' + event.currentTarget.id)
     var req = {
       url: baseUrl + 'users/' + value,
       beforeSend: setHeader
@@ -23,7 +22,9 @@ $('.btn').on('click', function (event) {
     $.ajax(req)
       .done(function (data) {
         $('#' + load + ' .progress').remove()
-      console.log(data)
+        var str = JSON.stringify(data, undefined, 2)
+        $('#' + card).append($('<pre>').text(str).addClass('code'))
+        console.log(data)
       })
       .fail(function (error) {
         console.log(error)
