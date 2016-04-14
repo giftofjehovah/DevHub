f5f2c79cdbd0c43ef05e6ce7a5cbd97e4377aad7
ozy8 <owzhiyin@Zhiyin-Ows-MacBook-Air.local>
2016-04-14 11:31:22 +0800

    change companies.ejs

--- controllers/companiesController.js
+++ controllers/companiesController.js
@@ -1,21 +1,21 @@
 // INDEX
 const Company = require('../models/company')
 
-function getAll (request, response) {
+function getAll (req, res) {
   Company.find(function (error, companies) {
-    if (error) return response.json({message: 'Company not found'})
+    if (error) return res.json({message: 'Company not found'})
     // response.send('Companies displayed')
-    response.json({companies: companies})
+    res.render('companies', {companies: companies})
   })
 }
 
 // SHOW
-function getCompany (request, response) {
-  var id = request.params.id
+function getCompany (req, res) {
+  var id = req.params.id
   Company.findById({_id: id}, function (error, company) {
-    if (error) response.json({message: 'Could not find company b/c:' + error})
+    if (error) res.json({message: 'Could not find company b/c:' + error})
     // response.send('Company displayed')
-    response.json({company: company})
+    res.json({company: company})
   })
 }
 
